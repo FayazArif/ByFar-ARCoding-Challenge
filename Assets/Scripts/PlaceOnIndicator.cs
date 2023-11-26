d9edd845c077c093f8a0c2a829c54d769a9cdb81
FayazArif <126565144+FayazArif@users.noreply.github.com>
2023-11-26 14:42:52 +0530

    Initial push

---
+++ Assets/Scripts/PlaceOnIndicator.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlaceOnIndicator : MonoBehaviour
+{
+    [SerializeField]
+    GameObject placementIndication;
+    
+    GameObject spawnedObject;
+
+    public bool markerstatus = false;
+    public bool modelStatus = false;
+
+    ARRaycastManager aRRaycastManager;
+    List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    [SerializeField]
+    DownloadAssetBundle downloadasset;
+
+    private void Awake()
+    {
+        aRRaycastManager = GetComponent<ARRaycastManager>();
+        placementIndication.SetActive(true);
+
+    }
+
+    private void Start()
+    {
+        downloadasset = GetComponent<DownloadAssetBundle>();
+    }
+
+    private void Update()
+    {
+        if(aRRaycastManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitpose = hits[0].pose;
+            placementIndication.transform.SetPositionAndRotation(hitpose.position, hitpose.rotation);
+
+            if(!placementIndication.activeInHierarchy && markerstatus)
+            {
+                placementIndication.SetActive(true);
+            }
+            else if(placementIndication.activeInHierarchy && !markerstatus)
+            {
+                placementIndication.SetActive(false);
+            }
+        }
+    }
+
+    public void EnableDisablePlacedMarker()
+    {
+        if (!markerstatus)
+        {
+            markerstatus = true;
+        }
+        else
+            markerstatus = false;
+    }
+
+    public void PlacedObject()
+    {
+        if (!placementIndication.activeInHierarchy)
+            return;
+
+        if (spawnedObject == null)
+        {
+            spawnedObject = Instantiate(downloadasset.InstantiateGameObjFromServer(), placementIndication.transform.position, placementIndication.transform.rotation);
+        }
+        else
+        {
+            spawnedObject.transform.SetPositionAndRotation(placementIndication.transform.position, placementIndication.transform.rotation);
+        }
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitScene()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/UIAnimController.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAnimController : MonoBehaviour
+{
+    public GameObject[] colors;
+   // public Sprite[] openAndCloseICon;
+    public Image selectedIconPlacement;
+    public Vector3[] selectedcolorposition;
+    public bool isColorEnable = false;
+    bool isDefaultColor = true;
+    Color defaultcarcolor = Color.green;
+    [SerializeField]
+    Material primaryColor;
+    Image openandclosebutton;
+    Image selectedIcon;
+
+    private void Start()
+    {
+        primaryColor.color = Color.black;
+        openandclosebutton = GetComponent<Image>();
+        
+    }
+
+    
+
+    public void EnableColors()
+    {
+        if(!isColorEnable)
+        {
+            foreach (GameObject g in colors)
+            {
+                g.SetActive(true);
+            }
+            isColorEnable = true;
+        }
+        else
+        {
+            foreach (GameObject g in colors)
+            {
+                g.SetActive(false);
+            }
+            isColorEnable = false;
+        }
+    }
+
+    public void ChangeRedColor()
+    {
+        isDefaultColor = true;
+        if (isDefaultColor)
+        {
+            if (primaryColor.color != Color.red)
+            {
+                primaryColor.color = Color.red;
+                isDefaultColor = false;
+            }
+            else
+            {
+                return;
+            }
+
+        }
+    }
+        
+    public void ChangeBlueColor()   
+    {
+        isDefaultColor = true;
+        if (isDefaultColor)
+        {
+            if (primaryColor.color != Color.blue)
+            {
+                primaryColor.color = Color.blue;
+                isDefaultColor = false;
+            }
+        }
+    }
+
+    public void ChangeYellowColor()
+    {
+        isDefaultColor = true;
+        if (isDefaultColor)
+        {
+            if (primaryColor.color != Color.gray)
+            {
+                primaryColor.color = Color.gray;
+                isDefaultColor = false;
+            }
+        }
+    }
+
+    public void ChangeBlackColor()
+    {
+        isDefaultColor = true;
+        if (isDefaultColor)
+        {
+            if (primaryColor.color != Color.black)
+            {
+                primaryColor.color = Color.black;
+                isDefaultColor = false;
+            }
+        }
+    }
+
+}
+
